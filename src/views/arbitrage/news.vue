e980eba8a57bd8f91595d8f79bbbe3a203debc2a
李大兵 <bzero1982@gmail.com>
2023-12-08 20:13:54 +0800

    优化编辑框

--- src/views/arbitrage/news.vue
+++ src/views/arbitrage/news.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative">
+  <div class="relative flex">
     <textarea
       v-model="message"
       placeholder="填入要编辑的内容"
@@ -7,13 +7,18 @@
       class="textarea-focus"
       style="
         box-sizing: border-box;
-        width: 1600px;
+        width: 800px;
         min-height: 300px;
         padding: 20px;
         border: 3px solid lightblue;
       "
       @input="autoGrow"
     ></textarea>
+    <div
+      class="ml-4 p-4 border-2 border-lightblue"
+      style="box-sizing: border-box; width: 800px; min-height: 300px; overflow: auto"
+      v-html="formattedMessage"
+    ></div>
     <a-button class="mt-4" @click="save">保存</a-button>
   </div>
 </template>
@@ -26,6 +31,12 @@
       return {
         message: '',
       };
+    },
+    computed: {
+      formattedMessage() {
+        // 在这里添加你的格式化逻辑
+        return this.message;
+      },
     },
     async created() {
       const response = await axios.get('http://api.chatools.net/v1/arbitrage_news/');
